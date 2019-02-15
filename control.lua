405840d0b42924ecd33180e7b3edf05020684bda
jinyz <yuncheolkim@163.com>
2019-02-15 22:48:19 +0800

    remove log

--- control.lua
+++ control.lua
@@ -17,12 +17,10 @@ function deepcopy(orig)
     if orig_type == 'table' then
         copy = {}
         for orig_key, orig_value in next, orig, nil do
-            log(orig_key.." ")
             copy[deepcopy(orig_key)] = deepcopy(orig_value)
         end
         setmetatable(copy, deepcopy(getmetatable(orig)))
     else -- number, string, boolean, etc
-        log(orig)
         copy = orig
     end
     return copy

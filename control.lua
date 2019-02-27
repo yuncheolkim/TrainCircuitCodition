195c5959b694628a170d9ffd85f62c82159df985
jinyz <yuncheolkim@163.com>
2019-02-27 22:33:56 +0800

    添加 N信号条件, 优先级大于M

--- control.lua
+++ control.lua
@@ -1,4 +1,6 @@
-isDebug = false
+
+isDebug = true
+local band = bit32.band
 function _print(msg)
     if isDebug then
         
@@ -37,13 +39,11 @@ local function regEvent()
             for stopId, stopTrain in pairs(global.stopTrans) do
                 if stopTrain.train.valid then
                     _print("检查停止的火车:"..stopId)
-                    checkManual(stopTrain.train)
+                    checkManual(stopTrain.train, 0)
                 else
                     global.stopTrans[stopId] = nil
                 end
-                
             end
-            
             global.Tick.checkTick = 0
         end
         
@@ -53,40 +53,65 @@ local function regEvent()
         
         global.stopTrans[train.id] = {
             train = train,
-            manual = false,
+            manual = false, -- 这里表示是否有信号
             record = 0,
+            signalBit = 0,
             station = train.station,
             stationName = GetMainLocomotive(train),
             stationId = train.station.unit_number
         }
     end
     
-    local waitContion = {type = "circuit", compare_type = "and", condition = {comparator = "≠", first_signal = {type = "virtual", name = "signal-M"}, constant = 1}}
+    local WaitCondition = {type = "circuit", compare_type = "and", condition = {comparator = "≠", first_signal = {type = "virtual", name = "signal-M"}, constant = 1}}
+    local waitConditionOr = {type = "circuit", compare_type = "or", condition = {comparator = "≠", first_signal = {type = "virtual", name = "signal-N"}, constant = 1}}
     
-    function NewScheduleRecord(train)
+    function NewScheduleRecord(train, signalBit)
         local stopTrain = global.stopTrans[train.id]
         if stopTrain then
+            if not stopTrain.train.schedule then
+                -- 时刻表为空则返回
+                return
+            end
+            
             local oldSched = deepcopy(stopTrain.train.schedule)
             local oldRecord = oldSched.records[oldSched.current]
             _print("增加信号条件,当前站:"..oldSched.current)
-            local newRecord = {station = oldRecord.station, wait_conditions = {
-                
-            }}
+            local newRecord = {station = oldRecord.station, wait_conditions = {}}
             stopTrain.record = oldSched.current
             
+            if not oldRecord.wait_conditions then
+                return
+            end
             local wait_conditions = {}
             local i = 0
+            local isM = bit32.band(signalBit, 1) == 1
+            local isN = bit32.band(signalBit, 2) == 2
+            -- N signal
+            
             for key, v in pairs(oldRecord.wait_conditions) do
-                if i == 0 then
-                    table.insert(wait_conditions, v)
-                    table.insert(wait_conditions, waitContion)
-                elseif v.compare_type == 'or' then
-                    table.insert(wait_conditions, v)
-                    table.insert(wait_conditions, waitContion)
-                else
-                    table.insert(wait_conditions, v)
+                if not isSameCircuit(v, WaitCondition) and not isSameCircuit(v, waitConditionOr) then
+                    
+                    if i == 0 then
+                        
+                        table.insert(wait_conditions, v)
+                        -- M signal
+                        if isM then
+                            table.insert(wait_conditions, WaitCondition)
+                        end
+                    elseif v.compare_type == 'or' then
+                        table.insert(wait_conditions, v)
+                        if isM then
+                            table.insert(wait_conditions, WaitCondition)
+                        end
+                    else
+                        table.insert(wait_conditions, v)
+                    end
                 end
                 i = i + 1
+            end
+            
+            if isN then
+                table.insert(wait_conditions, waitConditionOr)
             end
             newRecord.wait_conditions = wait_conditions
             oldSched.records[oldSched.current] = newRecord
@@ -96,7 +121,7 @@ local function regEvent()
         
     end
     
-    function isSameCircuit(t1)
+    function isSameCircuit(t1, waitContion)
         if t1.type == waitContion.type and
             t1.compare_type == waitContion.compare_type and
             t1.condition.first_signal.type == waitContion.condition.first_signal.type and
@@ -104,7 +129,7 @@ local function regEvent()
             return true
         end
     end
-    function RemoveScheduleRecord(train)
+    function RecalcScheduleRecord(train, signalBit)
         local stopTrain = global.stopTrans[train.id]
         if stopTrain then
             local oldSched = deepcopy(stopTrain.train.schedule)
@@ -113,20 +138,30 @@ local function regEvent()
             end
             
             local removeCircuitCondition = oldSched.records[stopTrain.record]
-            if not removeCircuitCondition then
+            if not removeCircuitCondition or not removeCircuitCondition.wait_conditions then
                 return
             end
-            _print("删除信号条件,当前站:"..stopTrain.record)
+            _print("删除信号条件,当前站:"..stopTrain.record.." "..signalBit)
             local newRecord = {station = removeCircuitCondition.station, wait_conditions = {
             }}
             
             local wait_conditions = {}
+            local delM = bit32.band(signalBit, 1) ~= 1
+            local delN = bit32.band(signalBit, 2) ~= 2
+
             for key, v in pairs(removeCircuitCondition.wait_conditions) do
-                if not isSameCircuit(v) then
-                    _print(v.type .. " ~~~")
+
+                if not delM and not delN then
+                    table.insert(wait_conditions, v)
+                elseif delM and not delN and not isSameCircuit(v, WaitCondition) then
+                    table.insert(wait_conditions, v)
+                elseif not delM and delN and not isSameCircuit(v, waitConditionOr) then
+                    table.insert(wait_conditions, v)
+                elseif not isSameCircuit(v, WaitCondition) and not isSameCircuit(v, waitConditionOr) then
                     table.insert(wait_conditions, v)
                 end
             end
+            
             newRecord.wait_conditions = wait_conditions
             oldSched.records[stopTrain.record] = newRecord
             local newSched = {current = oldSched.current, records = oldSched.records}
@@ -135,7 +170,8 @@ local function regEvent()
         
     end
     
-    function checkManual(train)
+    -- trainState 0: 在站台, 1:进站, 2:出站
+    function checkManual(train, trainState)
         local stopedTrain = global.stopTrans[train.id]
         
         if not stopedTrain then
@@ -145,36 +181,54 @@ local function regEvent()
             global.stopTrans[train.id] = nil
             return
         end
+
+        -- 火车离开站台, 直接清除所有信号条件
+        if trainState == 2 then
+            _print("火车离开站台 重新计算信号!! ")
+            RecalcScheduleRecord(stopedTrain.train, 0)
+            return
+        end
         
         local signals = stopedTrain.station.get_merged_signals()
-        local isManual = false
+        local signalBit = 0;
+        local oldSignalBit = stopedTrain.signalBit
+        
         if signals then
             for _, v in pairs(signals) do
                 if v.signal.type == "virtual" and v.signal.name == "signal-M" and v.count == 1 then
-                    _print("设置为手动 "..v.count)
-                    isManual = true
+                    _print("设置信号M "..v.count)
+                    signalBit = bit32.bor(signalBit, 1)
+                end
+                if v.signal.type == "virtual" and v.signal.name == "signal-N" and v.count == 1 then
+                    _print("设置信号N "..v.count)
+                    signalBit = bit32.bor(signalBit, 2)
                 end
                 
             end
             
-            if not stopedTrain.manual and isManual then
+            if stopedTrain.signalBit ~= signalBit then
+                -- 新加了N
+                if bit32.band(signalBit, 2) == 2 and bit32.band(stopedTrain.signalBit, 2) ~= 2 then
+                    NewScheduleRecord(stopedTrain.train, signalBit)
+                end
+                
                 -- 设置为手动
-                _print("设置为手动!! ")
-                stopedTrain.manual = true
-                NewScheduleRecord(stopedTrain.train)
+                _print("添加信号 ")
+                stopedTrain.signalBit = signalBit
+                NewScheduleRecord(stopedTrain.train, signalBit)
             end
         end
         
-        if not isManual and stopedTrain.manual then
-            _print("设置为自动!! ")
-            RemoveScheduleRecord(stopedTrain.train)
+        if oldSignalBit ~= signalBit then
+            _print("重新计算信号!! ")
+            RecalcScheduleRecord(stopedTrain.train, stopedTrain.signalBit)
         end
     end
     local function TrainArrives(train)
         _print("火车进入"..train.id)
         
         creteStop(train)
-        checkManual(train)
+        checkManual(train, 1)
         
     end
     local function TrainLeaves(oldState, train)
@@ -182,11 +236,10 @@ local function regEvent()
         
         local stopedTrain = global.stopTrans[train.id]
         
-        if stopedTrain and stopedTrain.manual then
-            checkManual(stopedTrain.train)
-            global.stopTrans[train.id] = nil
+        if stopedTrain and stopedTrain.signalBit ~= 0 then
+            checkManual(stopedTrain.train, 2)
         end
-        
+        global.stopTrans[train.id] = nil
     end
     
     function OnTrainStateChanged(event)
